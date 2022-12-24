560ae5c77893733a743b5b58a5ff910eef7a115f
Jhonathas <jhonathassouza8@gmail.com>
2022-12-24 19:44:08 -0300

    Estudando sobre animacao de scroll suave com JS

---
+++ ScrollSuave/js/app.js
@@ -0,0 +1,61 @@
+// Identificar o clique no menu
+// Verificar o item que foi clicado e fazer referência com o alvo
+// Verificar a distância entre o alvo e o topo
+// Animar o scroll até o alvo
+
+
+$('nav a').click(function(e){ //selecion meu nav do html para modificalo
+	e.preventDefault(); //Tira função padrao do hmtl
+	var id = $(this).attr('href'); //attr puxa o atributo
+		targetOffset = $(id).offset().top, 
+		menuHeight = $('nav').innerHeight();//Definindo qual o tamanho do nav para poder calcular a posição sozinho
+		$('html, body').animate({ //Animando o scroll
+			scrollTop: targetOffset - menuHeight
+		}, 300);
+});
+
+
+
+
+
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Animação ao Scroll
+(function(){
+	var $target = $('.anime'),
+			animationClass = 'anime-start',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
